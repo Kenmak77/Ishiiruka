dca3621be446ea1b4f356aa5cb06c9ebbb0bd59e
Kenmak77 <xelradark@gmail.com>
2025-06-14 16:26:29 +0200

    Update Version.cpp

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -9,7 +9,7 @@
 
 namespace Common
 {
-#define VERSION_STR FPM v3.1.2 P+FR 1.3.2" 
+#define VERSION_STR "FPM v3.1.2 P+FR 1.3.2" 
 #ifdef _DEBUG
 #define BUILD_TYPE_STR "Debug "
 #elif defined DEBUGFAST
