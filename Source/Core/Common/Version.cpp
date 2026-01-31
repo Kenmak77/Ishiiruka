8d35ac3a77bd4eb322e77735dc7179fb3368a79a
Kenmak77 <xelradark@gmail.com>
2026-01-31 16:06:06 +0100

    change version

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -9,7 +9,7 @@
 
 namespace Common
 {
-#define VERSION_STR "P+ v3.1.5 P+FR 1.4.8" 
+#define VERSION_STR "P+ v3.1.5 P+FR 1.4.9" 
 #ifdef _DEBUG
 #define BUILD_TYPE_STR "Debug "
 #elif defined DEBUGFAST
